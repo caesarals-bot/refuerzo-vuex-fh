20d641cd68284d8647cbb9607625d2f21e43b434
cesar londoño <slac_@hotmail.com>
2021-10-29 00:15:34 -0300

    read state - mutations

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
- 
+  <counter />
 </template>
 
 <script>
+import { defineAsyncComponent } from "vue";
 
 
 export default {
   name: 'App',
   components: {
-   
+    Counter: defineAsyncComponent( () => import('@/components/counter.vue'))
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,19 @@ import { createStore } from 'vuex'
 export default createStore({
 
     state:{
-        count: 1
+        count: 1,
+        lastMutation: 'none'
+    },
+
+    mutations: {
+        increment( state ){
+            state.count++
+            state.lastMutation = 'increment'
+        },
+        incrementBy(state, val ){
+            state.count += val
+            state.lastMutation = 'incrementBy'
+        }
     }
 
 })
